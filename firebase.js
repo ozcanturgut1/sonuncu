546148b6e12add3e91e02dc4b2462e575039052e
ozcanturgut1 <128786516+ozcanturgut1@users.noreply.github.com>
2024-10-09 12:52:13 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCzX3tGsTWHkbYMJSd4MVIJuq6SWhYwxE0",
+  authDomain: "curious-nucleus-392008.firebaseapp.com",
+  projectId: "curious-nucleus-392008",
+  storageBucket: "curious-nucleus-392008.appspot.com",
+  messagingSenderId: "741263300188",
+  appId: "1:741263300188:web:c0a3233b0d8667a9667899",
+  measurementId: "G-95P27PE17K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
